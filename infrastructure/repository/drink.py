72768e4b2259e30bd5ff29f07a23806a57db1283
Keito Omura <1119kate@gmail.com>
2020-02-25 11:24:58 +0900

    Post Slack channel

--- infrastructure/repository/drink.py
+++ infrastructure/repository/drink.py
@@ -1,6 +1,10 @@
 from domain.model import Drink
 from domain.repository import DrinkRepository
+from slackweb import Slack
+from os import environ
 
 class DrinkRepositoryImpl(DrinkRepository):
   def serve_drink(self, drink: Drink):
+    slack = Slack(url=environ["SLACK_URL"])
+    slack.notify(text=drink.get_name() + "!")
     print(drink.get_name() + "!")
